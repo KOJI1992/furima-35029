87adbc363adcec6e98ca895f155ea87763f097f7
KOJI1992 <qazwsx.1928.edcrfv@gmail.com>
2021-04-09 17:59:02 +0900

    テストコードの実装

--- spec/factories/order_delivery.rb
+++ spec/factories/order_delivery.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     address { '千代田区' }
     building { '柳ビル' }
     telephone_number {Faker::Number.leading_zero_number(digits: 11)}
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
