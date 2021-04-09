7ed829560d4091313f245878a7166205e3c8b1ba
KOJI1992 <qazwsx.1928.edcrfv@gmail.com>
2021-04-09 19:58:35 +0900

    商品購入機能の再修正

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -6,7 +6,7 @@ class OrderDelivery
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
     validates :city
     validates :address
-    validates :telephone_number, format: { with: /\A[0-9]{11}\z/, message: 'Input only number' }
+    validates :telephone_number, length: { maximum: 11 }, format: { with: /\A[0-9]+\z/, message: 'Input only number' }
     validates :user_id
     validates :item_id
     validates :token
